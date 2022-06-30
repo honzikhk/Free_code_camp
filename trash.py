826625c701f431935a4403ba910772a8559c8549
Honza <chudicekjan@gmail.com>
2022-06-30 17:42:33 +0200

    continue with probability calculator in trash.py

--- trash.py
+++ trash.py
@@ -1,11 +1,26 @@
-from budget_app_fcc import budget_app as budget
+import random
 
-food = budget.Category("Food")
-entertainment = budget.Category("Entertainment")
 
-food.deposit(900, "deposit")
-food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
-food.transfer(20, entertainment)
-print(food)
-expected = f"*************Food*************\ndeposit                 900.00\nmilk, cereal, eggs, bac -45.67\nTransfer to Entertainme -20.00\nTotal: 834.33"
-print(expected)
+class Hat:
+    def __init__(self, **kwargs):
+        self.contents = []
+        for key in kwargs:
+            for each in range(kwargs[key]):
+                self.contents.append(key)
+
+    def draw(self, number_of_balls):
+        if number_of_balls >= len(self.contents):
+            return self.contents
+        took = []
+        for each in range(number_of_balls):
+            took.append(self.contents.pop(random.randint(0, len(self.contents) - len(took))))
+        return took
+
+    def __str__(self):
+        return self.contents
+
+
+hat1 = Hat(red=5, orange=4, black=1, blue=0, pink=2, striped=9)
+print(hat1.contents)
+print(hat1.draw(2))
+
