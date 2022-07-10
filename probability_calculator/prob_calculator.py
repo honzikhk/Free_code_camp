35cbdba36f5ddd63e0cd3c44f796595fe0b69b44
honzikhk <93028167+honzikhk@users.noreply.github.com>
2022-07-10 11:57:17 +0200

    new feature restart_variables

--- probability_calculator/prob_calculator.py
+++ probability_calculator/prob_calculator.py
@@ -4,20 +4,24 @@ import random
 class Hat:
     def __init__(self, **kwargs):
         self.contents = []
+        self.took = []
         for key in kwargs:
             for each in range(kwargs[key]):
                 self.contents.append(key)
+        self.original_data = self.contents.copy()       # need this to restart_variables
+
+    def restart_variables(self):
+        self.contents = self.original_data.copy()
+        self.took.clear()
 
     def draw(self, number_of_balls):
         if number_of_balls >= len(self.contents):
             return self.contents
-        took = []
-        # balls = self.contents.copy()
         for i in range(number_of_balls):
             random_index = random.randint(0, len(self.contents) - 1)
             draw = self.contents.pop(random_index)
-            took.append(draw)
-        return took
+            self.took.append(draw)
+        return self.took
 
     def __str__(self):
         s = ""
@@ -33,6 +37,8 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
             expected_balls_lst.append(key)
     successful_experiments = 0
     for exp in range(num_experiments):
+        hat.restart_variables()
+        print(hat.contents)
         drawn = hat.draw(num_balls_drawn)
         cnt = 0
         for e in expected_balls_lst:
--- trash.py
+++ trash.py
@@ -1,7 +1,12 @@
 from probability_calculator import prob_calculator
 from probability_calculator.prob_calculator import Hat
 
-hat = prob_calculator.Hat(blue=3, red=2, green=6)
-probability = prob_calculator.experiment(hat=hat, expected_balls={"blue": 2, "green": 1}, num_balls_drawn=4, num_experiments=1000)
+hat = prob_calculator.Hat(red=5, blue=2)
+actual = hat.draw(2)
+
+
+print(len(hat.contents))
+
+
 
 
